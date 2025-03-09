42fae17dd96d9450ab1964190ebc88585038564f
YuriiBalandyuk <juraitclash@gmail.com>
2025-03-09 20:57:20 +0200

    Add comments

--- main.py
+++ main.py
@@ -23,37 +23,45 @@ def search_for_all_pairs():
     return search_list_pairs
 
 """
-Вибираю
+Знаходжу всі можливі добутки P=a×b та перевіряю які з них мають більше ніж одну пару (a,b).
 """
-def first_condition(inner_list_numbers):
-    product_to_pairs = {}
-    for a, b in inner_list_numbers:
-        product = a * b
-        if product not in product_to_pairs:
-            product_to_pairs[product] = []
-        product_to_pairs[product].append((a, b))
+def first_condition(inner_list_numbers_):
+    product_pairs = {}
+    for a, b in inner_list_numbers_:
+        value = a * b
+        if value not in product_pairs:
+            product_pairs[value] = []
+        product_pairs[value].append((a, b))
 
-    possible_prod_correct_pairs = {product: pairs for product, pairs in product_to_pairs.items() if len(pairs) > 1}
+    possible_prod_correct_pairs = {value: pairs for value, pairs in product_pairs.items() if len(pairs) > 1}
 
     return possible_prod_correct_pairs
 
-def second_condition(inner_list_numbers, inner_list_numbers_two):
-    sum_to_pairs = {}
-    for a, b in inner_list_numbers:
-        if a * b in inner_list_numbers_two:
-            if a + b not in sum_to_pairs:
-                sum_to_pairs[a + b] = []
-            sum_to_pairs[a + b].append((a, b))
-    return sum_to_pairs
+"""
+Знаходжу всі можливі суми S=a+b і залишу лише ті, які відповідають хоча б одному неоднозначному добутку.
+"""
+def second_condition(inner_list_numbers_, inner_list_numbers_two_):
+    sum_pairs = {}
+    for a, b in inner_list_numbers_:
+        if a * b in inner_list_numbers_two_:
+            if a + b not in sum_pairs:
+                sum_pairs[a + b] = []
+            sum_pairs[a + b].append((a, b))
+    return sum_pairs
 
-def third_condition(sum_to_pairs):
-    possible_sum_correct_pairs = {product: pairs for product, pairs in sum_to_pairs.items() if len(pairs) == 1}
+"""
+Залишу лише ті пари, де після того, як "Sum" визначив числа, "Prod" теж зможе їх дізнатися.
+"""
+def third_condition(sum_pairs):
+    possible_sum_correct_pairs = {value: pairs for value, pairs in sum_pairs.items() if len(pairs) == 1}
     return possible_sum_correct_pairs
 
-def fourth_condition(sum_to_pairs, possible_sums):
-    final_pairs = [pairs[0] for s, pairs in sum_to_pairs.items() if s in possible_sums]
-    return final_pairs
-
+"""
+Виводжу всі можливі пари (a,b), які відповідають умовам задачі.
+"""
+def fourth_condition(sum_pairs, possible_sum):
+    final_numbers_pairs = [pairs for s, pairs in sum_pairs.items() if s in possible_sum]
+    return final_numbers_pairs
 
 inner_list_numbers= search_for_all_pairs()
 print(inner_list_numbers)
@@ -64,6 +72,6 @@ print(inner_list_numbers_two)
 inner_list_numbers_three = third_condition(inner_list_numbers_two)
 print(inner_list_numbers_three)
 inner_list_numbers_four = fourth_condition(inner_list_numbers_two, inner_list_numbers_three)
-print(inner_list_numbers_four)
+print("\n", "Final list: ", inner_list_numbers_four)
 
 
